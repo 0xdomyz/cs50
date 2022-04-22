1645ce45cbf1565e2a575c70791254f699ba6711
unknown <yzdominic@outlook.com>
2022-04-22 21:48:32 +1000

    add files

---
+++ http/code.js
@@ -0,0 +1,24 @@
+let counter = 1;
+
+counter = counter + 1
+conter += 1;
+counter++;
+
+if (x < y)
+{}
+else if (x > y)
+{}
+else
+{}
+
+while (true){
+
+}
+
+for (let i = 0; i < 3; i++)
+{
+
+}
+
+
+
