736556cce0e92c8bf8901c91a728bd3fe971bb03
unknown <yzdominic@outlook.com>
2022-04-27 22:45:02 +1000

    add files

--- flas/fro/app.py
+++ flas/fro/app.py
@@ -1,4 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+SPORTS = ["aaa","bbb","ccc"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/register", methods=["POST"])
+def greet():
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+        return render_template("failure.html")
+    return render_template("success.html")
 
 
 
