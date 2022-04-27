29d45320c447b7d0ff133cd98db206bce44d18da
unknown <yzdominic@outlook.com>
2022-04-27 20:38:48 +1000

    restructure flas

--- flas/hello/app.py
+++ flas/hello/app.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -9,6 +8,6 @@ def index():
 
 @app.route("/greet", methods=['POST'])
 def greet():
-    name = request.args.get("name", "world")
+    name = request.form.get("name", "world")
     return render_template("greet.html", name=name)
 
